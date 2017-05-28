95e74fd7e60e425461adb432f13466a51f835295
Drewclam <andrew.c.lam94@gmail.com>
2017-05-28 19:20:50 -0400

    Add signup page for stylist

--- thesis-app/src/app/registration/components/index.ts
+++ thesis-app/src/app/registration/components/index.ts
@@ -4,3 +4,4 @@ export * from './signin-form';
 export * from './signup-button';
 export * from './signup-customer-form';
 export * from './registration-header';
+export * from './signup-stylist-form';
