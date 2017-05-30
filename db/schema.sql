136d55e3dd69f07740e0674c3ecf5ffd8bc4988a
Parul bir <parul.bir@gmail.com>
2017-05-29 23:01:22 -0700

    initial commit of database, and api routes

---
+++ db/schema.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE IF NOT EXISTS `dingdong`;
+
+USE dingdong;
+
+
+CREATE TABLE IF NOT EXISTs `users_stylists` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `type` INTEGER NOT NULL,
+  `name` VARCHAR(35) NOT NULL,
+  `password` VARCHAR(35) NOT NULL,
+  `billingaddress` TEXT DEFAULT NULL,
+  `phonenumber` VARCHAR(30) DEFAULT NULL,
+  `email` VARCHAR(30) DEFAULT NULL,
+  `longitude` DOUBLE DEFAULT NULL,
+  `latitude` DOUBLE DEFAULT NULL,
+  `site_url` VARCHAR(35) DEFAULT NULL,
+  `gender` VARCHAR(8) DEFAULT NULL,
+  `image_url` VARCHAR(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `services` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `servicename` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `stylists_services` (
+  `id_services` INTEGER NOT NULL ,
+  `id_users_stylists` INTEGER NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS `bookings` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `id_users` INTEGER NOT NULL,
+  `id_stylists` INTEGER NOT NULL,
+  `isconfirmed` INTEGER NOT NULL,
+  `time` VARCHAR(45) NOT NULL,
+  `location` TEXT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+ALTER TABLE `stylists_services` ADD FOREIGN KEY (id_services) REFERENCES `services` (`id`);
+ALTER TABLE `stylists_services` ADD FOREIGN KEY (id_users_stylists) REFERENCES `users_stylists` (`id`);
+ALTER TABLE `Bookings` ADD FOREIGN KEY (id_users) REFERENCES `users_stylists` (`id`);
+ALTER TABLE `Bookings` ADD FOREIGN KEY (id_stylists) REFERENCES `users_stylists` (`id`);
+
+
+
+
