f5fed86f54db6ae42db65450c0fe488d79edaa0f
woonandez <matthew.allen.fernandez@gmail.com>
2017-05-31 16:47:32 -0700

    add dummy array and for each

--- db/seed.js
+++ db/seed.js
@@ -1,32 +1,270 @@
 var helpers = require('./helpers.js');
 var services = require('../locationServices.js')
 
-helpers.addUserStylist(0, 'Shari', 'ShariPassword', 'San Ramon, CA, CA', '967-890-7890', 'Shari.gmail.com', 'ShariStylists.com', 'F', 'update me', function(result) {
-  console.log('this is result from adding', result.insertId);
-  // Get id from result
-  var id = result.insertId;
-  var location = 'San Ramon, CA'
-  // get location points/add longitude and latitude in stylists/users profile in database based on location provided
-  services.getLocationPoints(location, function(points) {
-    var lat = points[0];
-    var lng = points[1];
-    helpers.addLocation(lat, lng, id, function() {
-      console.log(result)
-    });
-  });
-});
+var stylistArray = [
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+    {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  },
+  {
+    id: 0,
+    'username': 'Shari',
+    'password': 'ShariPassword',
+    'location': 'San Ramon, CA, CA',
+    'phoneNumber': '967-890-7890',
+    'email': 'Shari@gmail.com',
+    'website': 'ShariStylists.com',
+    'style': 'F',
+    'update': 'update me'
+  }
+]
 
-helpers.addUserStylist(0, 'Robin', 'RobinPassword', 'Walnut Creek, CA', '968-890-7890', 'Robin.gmail.com', 'RobinStylists.com', 'M', 'update me', function(result) {
-  console.log('this is result from adding', result.insertId);
-  // Get id from result
-  var id = result.insertId;
-  var location = 'Walnut Creek, CA'
-  // get location points/add longitude and latitude in stylists/users profile in database based on location provided
-  services.getLocationPoints(location, function(points) {
-    var lat = points[0];
-    var lng = points[1];
-    helpers.addLocation(lat, lng, id, function() {
-      console.log(result)
-    });
+stylistArray.forEach(stylist => {
+  console.log(stylist)
+})
+
+helpers.addUserStylist(id, username, password, location, phoneNumber, email, website, type, update, (result) => {
+  var id = result.insertId
+  services.getLocationPoints(location, (points) => {
+    let lat = points[0];
+    let lng = points[1];
+    helpers.addLocation(lat, lng, id, (res) => {
+      console.log(res);
+    })
   });
-});
+}
+
+// helpers.addUserStylist(0, 'Shari', 'ShariPassword', 'San Ramon, CA, CA', '967-890-7890', 'Shari.gmail.com', 'ShariStylists.com', 'F', 'update me', function(result) {
+//   console.log('this is result from adding', result.insertId);
+//   // Get id from result
+//   var id = result.insertId;
+//   var location = 'San Ramon, CA'
+//   // get location points/add longitude and latitude in stylists/users profile in database based on location provided
+//   services.getLocationPoints(location, function(points) {
+//     var lat = points[0];
+//     var lng = points[1];
+//     helpers.addLocation(lat, lng, id, function() {
+//       console.log(result)
+//     });
+//   });
+// });
+
+// helpers.addUserStylist(0, 'Robin', 'RobinPassword', 'Walnut Creek, CA', '968-890-7890', 'Robin.gmail.com', 'RobinStylists.com', 'M', 'update me', function(result) {
+//   console.log('this is result from adding', result.insertId);
+//   // Get id from result
+//   var id = result.insertId;
+//   var location = 'Walnut Creek, CA'
+//   // get location points/add longitude and latitude in stylists/users profile in database based on location provided
+//   services.getLocationPoints(location, function(points) {
+//     var lat = points[0];
+//     var lng = points[1];
+//     helpers.addLocation(lat, lng, id, function() {
+//       console.log(result)
+//     });
+//   });
+// });
