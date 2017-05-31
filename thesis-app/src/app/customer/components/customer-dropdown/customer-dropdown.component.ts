3ebec296d46f55decf3f77fbb3e7d590c8b772e1
akcays <ilkesedefakcay@gmail.com>
2017-05-31 02:32:14 -0700

    Implement onSelect method to redirect the selected page

--- thesis-app/src/app/customer/components/customer-dropdown/customer-dropdown.component.ts
+++ thesis-app/src/app/customer/components/customer-dropdown/customer-dropdown.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { DropdownItem } from './dropdown-item';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'customer-dropdown',
   templateUrl: './customer-dropdown.component.html'
 })
-export class CustomerDropDownComponent {
 
+export class CustomerDropDownComponent {
+  selected: string;
+  open: boolean;
+
+  items = [
+    { value: 'Home' },
+    { value: 'Messages' },
+    { value: 'Settings' },
+    { value: 'Logout' },
+  ];
+
+  constructor(private router: Router) {}
+
+  public onSelect(item) {
+    this.selected = item;
+    console.log(this.selected);
+    switch (this.selected){
+      case "Home":
+        this.router.navigate(["/home"]);
+        break;
+      case "Messages":
+        console.log('router', this.router)
+        this.router.navigate(["/messages"]);
+        break;
+      case "Settings":
+        this.router.navigate(["/customerProfile"]);
+        break;
+      case "Logout":
+        this.router.navigate(["/login"]);
+        break;
+    }
+  }
 }
