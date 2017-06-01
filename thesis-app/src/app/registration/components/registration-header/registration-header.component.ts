b4365f85ef57a719927cd6b0aeb2ac4f2c474ff8
woonandez <matthew.allen.fernandez@gmail.com>
2017-05-31 21:02:23 -0700

    add bootstrap to the login page

--- thesis-app/src/app/registration/components/registration-header/registration-header.component.ts
+++ thesis-app/src/app/registration/components/registration-header/registration-header.component.ts
@@ -3,8 +3,8 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'registration-header',
   template: `
-    <h1>Ding Dong: It's Cut Time</h1>
-    <div class="page-header">
+    <div class="page-header" style="margin: auto; text-align: center">
+      <h1>Ding Dong: It's Cut Time</h1>
       <h2>Please Log in</h2>
     </div>
   `
