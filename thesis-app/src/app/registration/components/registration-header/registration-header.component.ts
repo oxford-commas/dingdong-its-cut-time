11705fd53b3a3931ee9301cd25a300a76283943d
woonandez <matthew.allen.fernandez@gmail.com>
2017-05-31 20:42:28 -0700

    add styling to the header

--- thesis-app/src/app/registration/components/registration-header/registration-header.component.ts
+++ thesis-app/src/app/registration/components/registration-header/registration-header.component.ts
@@ -3,8 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'registration-header',
   template: `
-    <h1>{{headerText}}</h1>
-    <div>{{logo}}</div>
+    <h1>Ding Dong: It's Cut Time</h1>
+    <div class="page-header">
+      <h2>Please Log in</h2>
+    </div>
   `
 })
 export class RegistrationHeaderComponent {
