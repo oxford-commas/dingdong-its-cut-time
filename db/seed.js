ed7c2de7655b2946bbfc8b7d41d64b9d20b630e9
woonandez <matthew.allen.fernandez@gmail.com>
2017-05-31 17:06:10 -0700

    cable car row

--- db/seed.js
+++ db/seed.js
@@ -15,79 +15,79 @@ var stylistArray = [
   },
   {
     id: 0,
-    'username': 'Shari',
-    'password': 'ShariPassword',
-    'location': 'San Ramon, CA, CA',
-    'phoneNumber': '967-890-7890',
-    'email': 'Shari@gmail.com',
-    'website': 'ShariStylists.com',
+    'username': 'Mateo',
+    'password': 'MateoPassword',
+    'location': 'San Mateo, CA, CA',
+    'phoneNumber': '967-123-7890',
+    'email': 'mateo@gmail.com',
+    'website': 'MateoStyles.com',
     'style': 'F',
     'update': 'update me'
   },
   {
     id: 0,
-    'username': 'Shari',
-    'password': 'ShariPassword',
-    'location': 'San Ramon, CA, CA',
-    'phoneNumber': '967-890-7890',
-    'email': 'Shari@gmail.com',
-    'website': 'ShariStylists.com',
-    'style': 'F',
+    'username': 'Ric Flair',
+    'password': 'WOO',
+    'location': 'Brewer Subdivision, Hillsborough, CA',
+    'phoneNumber': '967-123-7890',
+    'email': 'rflair@gmail.com',
+    'website': 'ricflairwoo.com',
+    'style': 'M',
     'update': 'update me'
   },
   {
     id: 0,
-    'username': 'Shari',
-    'password': 'ShariPassword',
-    'location': 'San Ramon, CA, CA',
-    'phoneNumber': '967-890-7890',
-    'email': 'Shari@gmail.com',
-    'website': 'ShariStylists.com',
-    'style': 'F',
+    'username': 'mrhighland',
+    'password': 'highlandpassword',
+    'location': '1851 Lexington Ave, San Mateo, CA',
+    'phoneNumber': '967-890-1238',
+    'email': 'highlands@gmail.com',
+    'website': 'HighlandsSuper.com',
+    'style': 'P',
     'update': 'update me'
   },
   {
     id: 0,
-    'username': 'Shari',
-    'password': 'ShariPassword',
-    'location': 'San Ramon, CA, CA',
-    'phoneNumber': '967-890-7890',
-    'email': 'Shari@gmail.com',
-    'website': 'ShariStylists.com',
-    'style': 'F',
+    'username': 'redwood',
+    'password': 'redwoodpassword',
+    'location': 'Redwood Shores, Redwood City, CA',
+    'phoneNumber': '832-592-7890',
+    'email': 'redwood@gmail.com',
+    'website': 'redwoodshores.com',
+    'style': 'P',
     'update': 'update me'
   },
   {
     id: 0,
-    'username': 'Shari',
-    'password': 'ShariPassword',
-    'location': 'San Ramon, CA, CA',
-    'phoneNumber': '967-890-7890',
-    'email': 'Shari@gmail.com',
-    'website': 'ShariStylists.com',
-    'style': 'F',
+    'username': 'Big Bess',
+    'password': 'BigBessington',
+    'location': '506 N Delaware St, San Mateo, CA',
+    'phoneNumber': '650-558-2399',
+    'email': 'someemail@gmail.com',
+    'website': 'sanmateohigh.org',
+    'style': 'P',
     'update': 'update me'
   },
   {
     id: 0,
-    'username': 'Shari',
-    'password': 'ShariPassword',
-    'location': 'San Ramon, CA, CA',
-    'phoneNumber': '967-890-7890',
-    'email': 'Shari@gmail.com',
-    'website': 'ShariStylists.com',
-    'style': 'F',
+    'username': 'Dragons Gate',
+    'password': 'dragonsgate',
+    'location': 'Bush St & Grant Ave., San Francisco, CA',
+    'phoneNumber': '503-321-7890',
+    'email': 'dragonsgate@gmail.com',
+    'website': 'welcometothedragon.com',
+    'style': 'T',
     'update': 'update me'
   },
   {
     id: 0,
-    'username': 'Shari',
-    'password': 'ShariPassword',
-    'location': 'San Ramon, CA, CA',
-    'phoneNumber': '967-890-7890',
-    'email': 'Shari@gmail.com',
-    'website': 'ShariStylists.com',
-    'style': 'F',
+    'username': 'cablecar',
+    'password': 'cablecar',
+    'location': '1201 Mason St, San Francisco, CA',
+    'phoneNumber': '415-474-1887',
+    'email': 'cablecablecable@gmail.com',
+    'website': 'cablecarmuseum.org',
+    'style': 'P',
     'update': 'update me'
   },
   {
@@ -234,8 +234,8 @@ stylistArray.forEach(stylist => {
     stylist.email,
     stylist.website,
     stylist.type,
-    stylist.update, (res) => {
-      var id = result.insertId;
+    stylist.update, (resultFromServer) => {
+      var id = resultFromServer.insertId;
       services.getLocationPoints(stylist.location, (points) => {
         let lat = points[0];
         let lng = points[1];
@@ -243,46 +243,5 @@ stylistArray.forEach(stylist => {
           console.log(res);
         })
       })
-    }
-})
-
-// helpers.addUserStylist(id, username, password, location, phoneNumber, email, website, type, update, (result) => {
-//   var id = result.insertId
-//   services.getLocationPoints(location, (points) => {
-//     let lat = points[0];
-//     let lng = points[1];
-//     helpers.addLocation(lat, lng, id, (res) => {
-//       console.log(res);
-//     })
-//   });
-// });
-
-// helpers.addUserStylist(0, 'Shari', 'ShariPassword', 'San Ramon, CA, CA', '967-890-7890', 'Shari.gmail.com', 'ShariStylists.com', 'F', 'update me', function(result) {
-//   console.log('this is result from adding', result.insertId);
-//   // Get id from result
-//   var id = result.insertId;
-//   var location = 'San Ramon, CA'
-//   // get location points/add longitude and latitude in stylists/users profile in database based on location provided
-//   services.getLocationPoints(location, function(points) {
-//     var lat = points[0];
-//     var lng = points[1];
-//     helpers.addLocation(lat, lng, id, function() {
-//       console.log(result)
-//     });
-//   });
-// });
-
-// helpers.addUserStylist(0, 'Robin', 'RobinPassword', 'Walnut Creek, CA', '968-890-7890', 'Robin.gmail.com', 'RobinStylists.com', 'M', 'update me', function(result) {
-//   console.log('this is result from adding', result.insertId);
-//   // Get id from result
-//   var id = result.insertId;
-//   var location = 'Walnut Creek, CA'
-//   // get location points/add longitude and latitude in stylists/users profile in database based on location provided
-//   services.getLocationPoints(location, function(points) {
-//     var lat = points[0];
-//     var lng = points[1];
-//     helpers.addLocation(lat, lng, id, function() {
-//       console.log(result)
-//     });
-//   });
-// });
+    });
+});
