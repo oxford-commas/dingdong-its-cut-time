f7c062708329671a5ace30a25661f1dd32a4c324
woonandez <matthew.allen.fernandez@gmail.com>
2017-06-02 11:11:38 -0700

    refactor nav component

--- thesis-app/src/app/customer/components/customer-navbar/customer-navbar.component.ts
+++ thesis-app/src/app/customer/components/customer-navbar/customer-navbar.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'customer-navbar',
-  templateUrl: './customer-navbar.component.html'
+  templateUrl: './customer-navbar.component.html',
+  styleUrls: ['./customer-navbar.component.css']
 })
 export class CustomerNavbarComponent {
   constructor() {
