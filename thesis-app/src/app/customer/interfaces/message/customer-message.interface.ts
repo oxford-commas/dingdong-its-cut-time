37e03aa370a3da20e93477d64d8ef3a61d736812
Drewclam <andrew.c.lam94@gmail.com>
2017-06-03 19:45:43 -0400

    Add location and change property names

--- thesis-app/src/app/customer/interfaces/message/customer-message.interface.ts
+++ thesis-app/src/app/customer/interfaces/message/customer-message.interface.ts
@@ -1,7 +1,8 @@
 export interface ICustomerMessage {
-  customer: string;
-  stylist: string;
+  id_users: string;
+  id_stylists: string;
   subjectHeading: string;
   body: string;
   time: number;
+  location: string;
 }
