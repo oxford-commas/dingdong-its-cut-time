68af4724961761f83a15288c94290b9e28a1f069
woonandez <matthew.allen.fernandez@gmail.com>
2017-06-05 11:42:35 -0700

    add signupstylist to the registration routes

--- thesis-app/src/app/registration/registration.routes.ts
+++ thesis-app/src/app/registration/registration.routes.ts
@@ -3,7 +3,8 @@ import { Routes } from '@angular/router';
 import {
   SignInFormComponent,
   SignUpFormComponent,
-  SignupUserComponent } from './components';
+  SignupUserComponent,
+  SignupStylistComponent } from './components';
 
 export const registrationRoutes: Routes = [
   {
@@ -22,5 +23,9 @@ export const registrationRoutes: Routes = [
   {
     path: 'signupuser',
     component: SignupUserComponent
+  },
+  {
+    path: 'signupstylist',
+    component: SignupStylistComponent
   }
 ]
