3aa81972384e92cd13abd17818c1cbd25be4d637
woonandez <matthew.allen.fernandez@gmail.com>
2017-06-05 11:36:08 -0700

    add boilerplate code for signupstylist component

---
+++ thesis-app/src/app/registration/components/signup-stylist/signup-stylist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'signup-stylist',
+  templateUrl: './signup-stylist.component.html',
+  styleUrls: ['./signup-stylist.component.css']
+})
+export Class SignupStylistComponent {
+  constructor() {}
+}
