2581d4da69442c8fddd282aed6151fef6f53a2e1
akcays <ilkesedefakcay@gmail.com>
2017-06-05 23:26:05 -0700

    Add methods to detect location and corrdinate changes

--- thesis-app/src/app/customer/components/customer-home/customer-home.component.ts
+++ thesis-app/src/app/customer/components/customer-home/customer-home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+
+import { CustomerNavbarComponent } from '../customer-navbar/';
+import { CustomerMapComponent } from '../customer-map/';
 
 import { RequestService } from '../../../services';
 import { LocationService } from '../../../services';
@@ -7,7 +10,8 @@ import { StateService } from '../../../services';
 
 @Component({
   selector: 'customer-home',
-  templateUrl: 'customer-home.component.html'
+  templateUrl: 'customer-home.component.html',
+  providers: [LocationService]
 })
 
 export class CustomerHomeComponent implements OnInit {
@@ -15,6 +19,9 @@ export class CustomerHomeComponent implements OnInit {
   public currentLocation: any;
   public customerProfile: any;
   public stylistsCloseToYou: any;
+  public searchLocation: string;
+  public latitude: number;
+  public longitude: number;
   userProfile: any
 
   constructor(
@@ -22,27 +29,49 @@ export class CustomerHomeComponent implements OnInit {
     private locationService: LocationService,
     private stylistService: StylistService,
     private stateService: StateService
-  ) {
-
-  }
+  ) {}
 
   ngOnInit() {
     this.customerProfile = this.stateService.customerProfile[0];
     this.isProfileFetched = true;
-    // // Default location inititialization to sanfrancisco
-    this.stylistService.getStylistsInLocation(this.stateService.customerProfile[0].billingaddress)
+    this.getLocationCoordinates(this.latitude, this.longitude);
+    this.getLocationFromCoordinates(this.latitude, this.longitude);
+    this.getStylistsAtLocation(this.currentLocation);
+  }
+
+  ngOnChanges() {
+    this.getStylistsAtLocation(this.searchLocation);
+    this.getLocationCoordinates(this.latitude, this.longitude);
+  }
+
+  getStylistsAtLocation(location: any) {
+    this.stylistService.getStylistsInLocation(this.currentLocation)
       .subscribe(data => {
         this.stylistsCloseToYou = data;
       }, err => console.log(err));
   }
 
-  getLocation() {
-    console.log('Clicked!');
-    this.currentLocation = this.locationService.getCurrentPosition()
+  onSearchLocationChange(location: string): void {
+    this.searchLocation = location;
+    console.log('New search location is:', this.searchLocation);
+  }
+
+  getLocationCoordinates(lat, lng) {
+    this.locationService.getCurrentPosition(lat, lng)
       .subscribe(res =>  {
-        console.log(res)
+        console.log(res);
+        this.currentLocation = res;
       });
     console.log('Location is:', this.currentLocation);
+  }
+
+  getLocationFromCoordinates(lat, lng) {
+    this.locationService.getLocationFromCoordinates(lat, lng)
+      .subscribe(res => {
+        console.log(res);
+        this.currentLocation = res;
+      });
+    console.log('Current location is:', this.currentLocation);
   }
 
 }
