c03b1c65229c68bedc0a8208e2f6214f1b72bafe
Andrew Lam <andrew.c.lam94@gmail.com>
2017-06-07 13:23:27 -0400

    Change port from 3000 to 4200

--- index.js
+++ index.js
@@ -346,6 +346,6 @@ app.get('*', function(req, res) {
  res.sendFile(path.join(__dirname, 'thesis-app/dist/index.html'));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(4200, function () {
+  console.log('Example app listening on port 4200!');
 });
