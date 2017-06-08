400a7a2325d11eff1ab6b4d123c1dcc4eae57ee3
akcays <ilkesedefakcay@gmail.com>
2017-06-07 20:01:36 -0700

    Delete config file

--- thesis-app/src/app/customer/config/config.service.ts
+++
@@ -1,9 +0,0 @@
-import {Injectable} from '@angular/core';
-
-@Injectable()
-export class ConstantService {
-  API_KEY: any;
-  constructor() {
-    this.API_KEY = 'AIzaSyDiVQjsDczrtUuAdadrUB2lFn39QEWK43A';
-  }
-}
