a3fcaeb247e1f2b008e5de965d502e3772ef46f3
Parul bir <parul.bir@gmail.com>
2017-06-09 16:44:38 -0700

    Added clearDB url

--- db/model.js
+++ db/model.js
@@ -1,11 +1,13 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'dingdong'
-});
+// var con = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'dingdong'
+// });
+
+var con = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
 
 con.connect();
 
