ac6443885a559a3552a2b4c5564013cbde80eae4
akcays <ilkesedefakcay@gmail.com>
2017-06-10 12:14:09 -0700

    Re-render close to you component when search location changes

--- thesis-app/src/app/customer/components/close-to-you/close-to-you.component.ts
+++ thesis-app/src/app/customer/components/close-to-you/close-to-you.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocationService } from '../../../services';
+import { StylistService } from '../../../services';
 
 @Component({
   selector: 'close-to-you',
@@ -6,5 +9,48 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./close-to-you.component.css']
 })
 export class CloseToYouComponent {
+    constructor(
+    private locationService: LocationService,
+    private stylistService: StylistService,
+    private router: Router
+  ) {}
+
   @Input() stylistInformation: any;
+  @Input() searchLocation: string;
+
+  // public currentLocation: string;
+  // public lng: number;
+  // public lat: number;
+  // public stylists: any
+
+  // ngOnInit() {
+  //   this.getLatLng((lat, lng) => this.getLocationFromCoordinates(lat, lng, (location) => this.getStylistsInLocation(location)));
+  // }
+
+  // getLatLng(next) {
+  //   this.locationService.getCurrentPosition(null, null)
+  //     .subscribe(res =>  {
+  //       this.lat = res.coords.latitude;
+  //       this.lng = res.coords.longitude;
+  //       next(this.lat, this.lng);
+  //     });
+  // }
+
+  // getStylistsInLocation(location: string) {
+  //   this.stylistService.getStylistsInLocation(location)
+  //     .subscribe(
+  //       data => this.stylists = data
+  //     , err => console.log(err)
+  //     );
+  // }
+
+  // getLocationFromCoordinates(lat, lng, next) {
+  //   this.locationService.getLocationFromCoordinates(lat, lng)
+  //     .subscribe(location => {
+  //       this.currentLocation = location;
+  //       this.searchLocation = this.currentLocation;
+  //       next(this.currentLocation);
+  //     }, err => console.log(err));
+  // }
+
 }
--- thesis-app/src/app/customer/components/customer-map/customer-map.component.ts
+++ thesis-app/src/app/customer/components/customer-map/customer-map.component.ts
@@ -21,7 +21,7 @@ export class CustomerMapComponent implements OnInit {
   public currentLocation: string;
   public lng: number;
   public lat: number;
-  public zoom: number = 14;
+  public zoom: number = 12;
 
   ngOnInit() {
     this.getLatLng((lat, lng) => this.getLocationFromCoordinates(lat, lng, (location) => this.getStylistsInLocation(location)));
@@ -39,25 +39,20 @@ export class CustomerMapComponent implements OnInit {
         this.lat = res.coords.latitude;
         this.lng = res.coords.longitude;
         next(this.lat, this.lng);
-        // console.log(`Latitude is: ${this.lat}, longitude is: ${this.lng}`);
       });
   }
 
   adjustMapViewForLocation(location: string) {
     this.locationService.getCoordinatesFromLocation(location)
       .subscribe(res => {
-        // console.log(res);
-        // console.log(JSON.stringify(res));
         this.lat = res.lat;
         this.lng = res.lng;
-        // console.log(`Latitude is: ${this.lat}, longitude is: ${this.lng}`);
       });
   }
 
   getStylistsInLocation(location: string) {
     this.stylistService.getStylistsInLocation(location)
       .subscribe(data => {
-        // console.log('Loc is', location);
         this.stylists = data;
         this.stylists.map(stylist => {
           stylist.label = {
@@ -81,15 +76,10 @@ export class CustomerMapComponent implements OnInit {
     this.router.navigateByUrl(`/stylistProfile/${id}`);
   }
 
-  clickedMarker(label: string, index: number) {
-    // console.log(`clicked the marker: ${label || index}`)
-  }
-
   // helper function to get coordinates of a point on map
   mapClicked($event: any) {
     let lat = $event.coords.lat;
     let lng = $event.coords.lng;
-    // console.log(`lat: ${lat}, lng: ${lng}`);
   }
 
   public stylists: any
