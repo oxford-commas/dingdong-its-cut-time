fb2c6041af3f655693954fe5d4d7232a7979d9d9
Parul bir <parul.bir@gmail.com>
2017-06-10 17:47:58 -0700

    Updated static files location

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'thesis-app/dist')));
+app.use(express.static('thesis-app/dist'));
 
 /// placeholder validation for auth 0//
 app.get('/api/validate/:username/:password', function(req, res) {
