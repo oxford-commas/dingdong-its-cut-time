b03f08eb63af18c9715cadee9edc37132f610017
Parul bir <parul.bir@gmail.com>
2017-06-10 17:43:12 -0700

    Updated route

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'thesis-app/dist')));
 
 /// placeholder validation for auth 0//
@@ -321,7 +322,7 @@ app.get('/api/streetaddress/:latlng', function(req, res) {
   });
 });
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
  res.sendFile('index.html');
 });
 
