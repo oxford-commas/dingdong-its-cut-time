8ec249309e20d0b6edd15bd5a2ff43d46ecc9d5f
Drewclam <andrew.c.lam94@gmail.com>
2017-06-12 16:42:59 -0400

    add Shari test customer to seed

--- db/seed.js
+++ db/seed.js
@@ -325,6 +325,14 @@ const booksingsArray = [
     isComplete: 0,
     time: '1:00pm',
     location: '2323 Lyon St, San Francisco'
+  },
+  {
+    id_users: 1,
+    id_stylists: 2,
+    isconfirmed: 0,
+    isComplete: 0,
+    time: '1:00pm',
+    location: '2323 Lyon St, San Francisco'
   }
 ]
 
