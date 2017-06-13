9576004b5564f9ffc8f26a14f3e53a7e849e9c4d
Drewclam <andrew.c.lam94@gmail.com>
2017-06-12 23:37:18 -0400

    Clean up code

--- thesis-app/src/app/customer/components/customer-home/customer-home.component.ts
+++ thesis-app/src/app/customer/components/customer-home/customer-home.component.ts
@@ -6,35 +6,29 @@ import { CustomerMapComponent } from '../customer-map/';
 import {
   LocationService,
   StylistService,
-  BookingService,
   StateService } from '../../../services';
 
 @Component({
   selector: 'customer-home',
   templateUrl: 'customer-home.component.html',
-  styleUrls: ['./customer-home.component.css'],
-  providers: [LocationService]
+  styleUrls: ['./customer-home.component.css']
 })
 
 export class CustomerHomeComponent implements OnInit {
-  public isProfileFetched = false;
-  public currentLocation: any;
-  public location: string = this.location || this.currentLocation;
-  public customerProfile: any;
-  public stylistsCloseToYou: any;
-  public searchLocation: string;
-  public latitude: number;
-  public longitude: number;
-
   constructor(
     private locationService: LocationService,
     private stylistService: StylistService,
     private stateService: StateService
   ) {}
 
+  public currentLocation: any;
+  public location: string = this.location || this.currentLocation;
+  public stylistsCloseToYou: any;
+  public searchLocation: string;
+  public latitude: number;
+  public longitude: number;
+
   ngOnInit() {
-    this.customerProfile = this.stateService.retrieveCustomer();
-    this.isProfileFetched = true;
     this.getLocationCoordinates((lat, lng) => this.getLocationFromCoordinates(lat, lng, (location) => this.updateCloseToYou(location)));
     this.searchLocation = this.location || this.currentLocation;
   }
--- thesis-app/src/app/customer/components/customer-navbar/customer-navbar.component.ts
+++ thesis-app/src/app/customer/components/customer-navbar/customer-navbar.component.ts
@@ -1,25 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { StylistService, LocationService, StateService } from '../../../services';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'customer-navbar',
   templateUrl: './customer-navbar.component.html',
   styleUrls: ['./customer-navbar.component.css']
 })
-export class CustomerNavbarComponent {
-  constructor (
-    private stylistService: StylistService,
-    private locationService: LocationService,
-    private stateService: StateService
-  ) {}
-
-  @Input() searchLocation: string;
-  @Output() locationChange = new EventEmitter();
-
-  public profile = this.stateService.retrieveCustomer();
-
-  public handleSearchLocationChange(newLocation) {
-    this.searchLocation = newLocation;
-    this.locationChange.emit(newLocation);
-  }
-}
+export class CustomerNavbarComponent {}
