771ad5f2fb48e84aee6983202b9780ec2d14803f
Drewclam <andrew.c.lam94@gmail.com>
2017-06-14 16:25:24 -0400

    Fix profile picture update

--- db/helpers.js
+++ db/helpers.js
@@ -54,9 +54,9 @@ var updateImage = function (imageUrl, id, callback) {
   });
 };
 
-var updateProfile = function(type, name, password, billingaddress, phonenumber, email, site_url, gender, image_url, aboutMe, id, callback) {
-  var sql = 'UPDATE users_stylists SET type = ?, name = ?, password = ?, billingaddress = ?, phonenumber = ?, email = ?, site_url = ?, gender = ?, image_url = ?, aboutMe = ? WHERE id = ?'
-  model.con.query(sql, [type, name, password, billingaddress, phonenumber, email, site_url, gender, image_url, aboutMe, id],function (err, result) {
+var updateProfile = function(type, name, password, billingaddress, phonenumber, email, site_url, gender, aboutMe, id, callback) {
+  var sql = 'UPDATE users_stylists SET type = ?, name = ?, password = ?, billingaddress = ?, phonenumber = ?, email = ?, site_url = ?, gender = ?, aboutMe = ? WHERE id = ?'
+  model.con.query(sql, [type, name, password, billingaddress, phonenumber, email, site_url, gender, aboutMe, id],function (err, result) {
     if (err) throw err;
     console.log("1 record updated");
     callback();
