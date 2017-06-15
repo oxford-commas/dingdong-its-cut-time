a79915c75c01b74ff8cf08678529fcec81932b0a
Drewclam <andrew.c.lam94@gmail.com>
2017-06-15 15:53:05 -0400

    Fix close to you picture

--- thesis-app/src/app/customer/components/booking/booking.component.ts
+++ thesis-app/src/app/customer/components/booking/booking.component.ts
@@ -1,32 +1,15 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
-import { RequestService } from '../../../services';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.css']
 })
-export class BookingComponent implements OnInit {
-  constructor(private requestService: RequestService) {}
-
+export class BookingComponent {
   @Input() name;
-  @Input() id_stylists;
+  @Input() image_url;
   @Input() time;
   @Input() phoneNumber;
   @Input() location;
   @Input() isPending: boolean = false;
-  public image_url;
-  public isPictureFetched = false;
-
-  ngOnInit() {
-    this.requestService.getStylistById(this.id_stylists)
-      .subscribe(
-        data => {
-          console.log('!!!!', data);
-          this.image_url = data.image_url;
-        },
-        err => console.log(err),
-        () => this.isPictureFetched = true
-      );
-  }
 }
