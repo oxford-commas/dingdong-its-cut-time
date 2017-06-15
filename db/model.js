b8226ad7d34fe0fa9908b35e6c9e4ee3ae02da78
Parul bir <parul.bir@gmail.com>
2017-06-15 15:37:43 -0700

    Pool set for model.js

--- db/model.js
+++ db/model.js
@@ -9,9 +9,20 @@ var con = mysql.createConnection({
 });
 */
 // console.log("URL: ", process.env.CLEARDB_DATABASE_URL);
-var con = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
 
-con.connect();
+var con = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
+
+con.getConnection(function(err, connection) {
+  // Use the connection
+  connection.query('SELECT something FROM sometable', function (error, results, fields) {
+    // And done with the connection.
+    connection.release();
+
+    // Handle error after the release.
+    if (error) throw error;
+
+    // Don't use the connection here, it has been returned to the pool.
+});
 
 module.exports.con = con;
 
