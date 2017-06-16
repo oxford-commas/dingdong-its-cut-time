ff1fa1828d95ebf239d4f56d3331aae47d1eaa2c
Parul bir <parul.bir@gmail.com>
2017-06-15 22:59:21 -0700

    Fixed images

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static('uploads'));
 
 /// placeholder validation for auth 0//
 app.get('/api/validate/:username/:password', function(req, res) {
