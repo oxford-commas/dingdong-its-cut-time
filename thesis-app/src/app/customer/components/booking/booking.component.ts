a2e62f59c65360b433dd02c196196f6796d04cb8
Drewclam <andrew.c.lam94@gmail.com>
2017-06-16 01:04:58 -0400

    Fix appointments

--- thesis-app/src/app/customer/components/booking/booking.component.ts
+++ thesis-app/src/app/customer/components/booking/booking.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 // import { BookingService } from '../../../booking.service';
 
 @Component({
@@ -6,20 +6,38 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.css']
 })
-export class BookingComponent implements OnInit {
+export class BookingComponent {
   // constructor(private bookingService: BookingService) {}
 
+  @Input() index;
   @Input() name;
   @Input() image_url;
   @Input() time;
   @Input() location;
   @Input() detail;
   @Input() date;
-  public email;
   @Input() phoneNumber;
   @Input() isPending: boolean = false;
+  @Output() handleDecline = new EventEmitter();
+  @Output() handleClearHistory = new EventEmitter();
+  @Output() handleCancel = new EventEmitter();
+  @Output() handleComplete = new EventEmitter();
+  @Output() handleCancelComplete = new EventEmitter();
+  @Output() handleAccept = new EventEmitter();
+  @Output() handleAskForPay = new EventEmitter();
+  @Output() handlePay = new EventEmitter();
 
-  ngOnInit() {
-    // this.boo
-  }
+  public email;
+  @Input() isAccept: boolean = false;
+  @Input() isDecline: boolean = false;
+  @Input() isHistory: boolean = false;
+  @Input() isConfirmed: boolean = false;
+  @Input() isPay: boolean = false;
+  @Input() isReady: boolean = false;
+  @Input() isCancel: boolean = false;
+  @Input() isCancelComplete: boolean = false;
+
+  @Input() stylistName;
+  @Input() bookingId;
+
 }
